50179fb4a6f390904fa57fa62373aa75efaae0fb
Keidrych Anton-Oates <keidrych@sotekton.com>
2019-03-12 15:00:18 +1000

    feat: nodejs container

--- node.nix
+++ node.nix
@@ -21,7 +21,7 @@ buildInfo = {
 		WorkingDir = "/opt/app";
   };
 	name = "sotekton/basal";
-	tag = if ver == null then "nodejs" else "nodejs${ver}";
+	tag = if ver == null then "nodejs" else "nodejs-${ver}_x";
 };
 
 # Base Image should contain only the essentials to run the application in a container.
