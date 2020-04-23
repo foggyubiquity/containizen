a3853529256e6233a00d5b59053181b86ce203a8
Keidrych <keidrych@tayloredtechnology.net>
2020-04-23 10:54:31 +1000

    feat: python vulnerbility scanning

--- languages/python.nix
+++ languages/python.nix
@@ -1,9 +1,10 @@
 { ver ? null
 , pkgs ? import (
-    fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixpkgs-unstable.tar.gz
+    fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/${pkgsPinned}.tar.gz"
   ) { config = { allowUnfree = true; }; }
+, pkgsPinned ? "nixpkgs-unstable"
+, vulnix ? null
 }:
-
 let
   #######################
   # Configuration       #
@@ -27,6 +28,8 @@ let
   #######################
   # Build Image Code    #
   #######################
-
 in
-pkgs.callPackage ../common.nix { inherit buildInfo pkgs language; }
+if vulnix == null then
+  pkgs.callPackage ../common.nix { inherit buildInfo pkgs language; }
+else
+  pkgs.callPackage ../vulnix/default.nix { inherit buildInfo pkgs language; }
