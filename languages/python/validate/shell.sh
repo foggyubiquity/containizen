e7a3cde7ae50de4636090d5e17da00dd6ccd6b01
ohgnoes <64242471+ohgnoes@users.noreply.github.com>
2020-06-18 14:44:32 -0700

    bash: ./containizen.pyz: /sbin/s6-env: bad interpreter: No such file or directory

--- languages/python/validate/shell.sh
+++ languages/python/validate/shell.sh
@@ -3,4 +3,4 @@ pip-sync requirements/prod.txt requirements/dev.txt
 
 # export APP=$(grep "name=" ./setup.py | cut -d"\"" -f2 | cut -d"'" -f2)
 pip install -r requirements/prod.txt --upgrade --target pkgs
-SOURCE_DATE_EPOCH=$(date +%s) shiv --compressed --python '/sbin/s6-env python3' --console-script main --output-file containizen.pyz --site-packages pkgs .
+SOURCE_DATE_EPOCH=$(date +%s) shiv --compressed --python '/bin/s6-env python3' --console-script main --output-file containizen.pyz --site-packages pkgs .
