ab4bffe7b1cafdd929b9918ae8a42a9c552ebea6
Keidrych <keidrych@tayloredtechnology.net>
2020-09-24 13:01:32 +1000

    chore: rectify GitHub Niv incompatibility

--- nix/default.nix
+++ nix/default.nix
@@ -1,20 +1,25 @@
 let
   sources = import ../nix/sources.nix;
-  callPackage = (import sources.nixpkgs { }).callPackage;
+  callPackage = (import sources.nixpkgs {}).callPackage;
 in
 import sources.nixpkgs {
   overlays = [
-    (final: prev: {
-      dockerTools = (import sources.pinnedDockerTools { }).dockerTools;
-      k6 = prev.k6.overrideAttrs (old: {
-        name = "patched-k6-${old.version}";
-        src = sources.k6;
-      });
-      goss = callPackage
-        ./goss.nix { inherit sources; };
-      s6-overlay = sources.s6-overlay;
-      vulnix = callPackage sources.vulnix { };
-    })
+    (
+      final: prev: {
+        dockerTools = (import sources.pinnedDockerTools {}).dockerTools;
+        k6 = prev.k6.overrideAttrs (
+          old: {
+            name = "patched-k6-${old.version}";
+            src = sources.k6;
+          }
+        );
+        goss = callPackage
+          ./goss.nix { inherit sources; };
+        s6-overlay = sources.s6-overlay;
+        vulnix = callPackage sources.vulnix {};
+        niv = (import sources.niv {}).niv;
+      }
+    )
   ];
   config = {
     allowUnfree = true;
